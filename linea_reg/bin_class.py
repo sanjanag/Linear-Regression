c7bc02bd9dbd831aa68fe6f39d12ee89af2fe370
Amartya <amartya@cse.iitk.ac.in>
2016-01-03 22:14:27 +0530

    SGD update

--- linea_reg/bin_class.py
+++ linea_reg/bin_class.py
@@ -2,15 +2,18 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import math
-
+train = int(sys.argv[3])
 class LogisticRegression(object):
 
     def __init__(self, trainfileName, testfileName):
         self.alpha = 0.000001
         self.features = 0
         self.read_data(trainfileName, testfileName)
-        self.Beta = np.matrix( np.zeros((self.features+1,1)))
-
+        self.Beta = np.random.normal(0,0.01,(self.features+1,1))
+        self.threshold = 0.0
+        self.iter = 1000000
+        self.mini = 5
+        self.l2 = 0.001
     def read_data(self, testfile, trainfile):
         testfile = open(testfile, 'r')
         trainfile = open(trainfile, 'r')
@@ -38,8 +41,8 @@ class LogisticRegression(object):
             self.Y_train[i,0] = s[0]
             i = i+1
 
-        self.X_train = np.matrix(self.X_train, dtype = int)
-        self.Y_train = np.matrix(self.Y_train, dtype = int)
+        #self.X_train = np.asarray(self.X_train, dtype = int)
+        #self.Y_train = np.asarray(self.Y_train, dtype = int)
         
         i = 0
         for line in lines_test:
@@ -54,77 +57,68 @@ class LogisticRegression(object):
                 self.X_test[i,j] = s[j]
             self.Y_test[i,0] = s[0]
             i = i+1
-        self.X_test = np.matrix(self.X_test)
-        self.Y_test = np.matrix(self.Y_test)
-
     def get_cost(self, a, b, size):
-        c = a*self.Beta
-        for i in range(c.size):
-            c[i,0] = 1/(1+math.exp(-c[i,0]))
-        d = np.matrix(np.ones((size,1))) - c
-        b1 = np.matrix(np.ones((size,1))) - b
-        for i in range(c.size):
-            if c[i,0] == 0:
-                c[i,0] = 999999
-            else:
-                c[i,0] = math.log(c[i,0])
-                
-        for i in range(d.size):
-            if d[i,0] == 0:
-                d[i,0] = 999999
-            else:
-                d[i,0] = math.log(d[i,0])
-        return (((b.getT()*c)[0,0] + (b1.getT()*d)[0,0])*(-1))/size
+        c = np.dot(a,self.Beta)
+        c = np.log(1/(1+np.exp(-c)))
+        d = np.log(1-c)
+        b1 = 1- b
+        #print np.sum(((b*c) + (b1*d))*(-1)),np.sum(self.Beta*self.Beta*self.l2/2)
+        return (np.sum(((b*c) + (b1*d))*(-1))+np.sum(self.Beta*self.Beta*self.l2/2))/size
 
     def get_Grad(self, X, Y):
-        a = X*self.Beta
-        for i in range(a.size):
-            a[i,0] = 1/(1+math.exp(-a[i,0]))
-        a = ((a-Y).getT())*X
-        return (a*self.lr).getT()
+        a = np.dot(X,self.Beta)
+        a = 1/(1+np.exp(-a))
+        c = np.repeat((a-Y),(self.features+1)).shape
+        a = np.repeat((a-Y),(self.features+1)).reshape(len(a),(self.features+1))
+        grad = a*X
+        
+        #print grad.shape
+        #print (np.mean(grad*self.lr,axis=0).reshape(self.features+1,1)+self.l2*self.Beta).shape
+        return (np.mean(grad*self.lr,axis=0)).reshape(self.features+1,1)+self.l2*self.Beta
         
     def train(self, X, Y, size):
         costList = []
         cost = self.get_cost(X,Y,size)
         print "initial cost ", cost
         prevcost = cost
-        self.lr = np.identity(self.features+1)
-        for i in range(self.features+1):
-            self.lr[i,i] = self.alpha/(self.trainsize)
+        self.lr = self.alpha
         i = 0
-
-        while(i < 10000000):
-            grad = self.get_Grad(X,Y)
+        n_batch = self.trainsize/self.mini
+        while(i < self.iter):
+            batch_index = i%n_batch
+            X_ind = X[self.mini*batch_index:self.mini*(batch_index+1)]
+            Y_ind = Y[self.mini*batch_index:self.mini*(batch_index+1)]
+            grad = self.get_Grad(X_ind,Y_ind)
+            #print "Updating"
+            #print self.Beta.shape,grad.shape
             self.Beta = self.Beta - grad
-            cost = self.get_cost(X,Y,size)
+            #print self.Beta.shape
+            #print 
+            cost = self.get_cost(X_ind,Y_ind,size)
             costList.append(cost)
-            if i%100 == 0:
+            if i%10000 == 0:
                 print cost
-            
             i = i+1
 
-            if(cost >= prevcost or (cost < prevcost and abs(cost - prevcost) < 0.0000001)):
-                print cost, prevcost
-                break
-            else:
-                prevcost = cost
+            #if(cost >= prevcost or (cost < prevcost and abs(cost - prevcost) < 0.0000001)):
+                #print cost, prevcost
+                #break
+            #else:
+            #    prevcost = cost
         return self.Beta, costList, i
 
     def test(self, X, Y, size, Beta):
-        a = X*Beta
-        for i in range(a.size):
-            a[i,0] = 1/(1+math.exp(-a[i,0]))
-        testval = np.matrix(np.ones((self.testsize,1)), dtype = int)
-        for i in range(testval.size):
-            if a[i,0] < 0.72:
-                testval[i,0] = 0
-        return self.get_accuracy(testval, Y)
+        a = np.dot(X,Beta)
+        a = 1/(1+np.exp(-a))
+        a[a<self.threshold]=0
+        a[a>=self.threshold]=1
+        return self.get_accuracy(a, Y)
     
 
     def get_accuracy(self, a,b):
         count = 0
         for i in range(a.size):
-            if(a[i,0] == b[i,0]):
+            if(a[i] == b[i]):
                 count = count + 1
         return ((float(count))/a.size)*100
     
@@ -132,23 +126,33 @@ trainf = sys.argv[1]
 testf = sys.argv[2]
 
 model = LogisticRegression(trainf, testf)
+if train == 1:
+    Beta, costList, n_epoch = model.train(model.X_train, model.Y_train, model.trainsize)
 
-#Beta, costList, n_epoch = model.train(model.X_train, model.Y_train, model.trainsize)
-
-#f = open("res.txt",'w')
-#for i in range(model.features+1):
-#    f.write("%s\n" % str(Beta[i,0]))
-#f.close()
-Beta = np.matrix(np.ones((model.features+1,1)))
-f = open("res.txt",'r')
-lines = f.readlines()
-lines= [float(x.strip('\n')) for x in lines]
-#print lines
-for i in range(model.features+1):
-    Beta[i,0] = lines[i]
+    f = open("res.txt",'w')
+    for i in range(model.features+1):
+        f.write("%s\n" % str(Beta[i,0]))
+    f.close()
+else:
+    Beta = np.ones((model.features+1,1))
+    f = open("res.txt",'r')
+    lines = f.readlines()
+    lines= [float(x.strip('\n')) for x in lines]
+    Beta = np.asarray(lines)
+max_accuracy  = 0
+best_thresh = 0
+model.threshold = 0.51
+for i in range(0,200):
+    accuracy  = model.test(model.X_train, model.Y_train, model.trainsize, Beta)
+    model.threshold += 0.001
+    if accuracy >max_accuracy:
+        max_accuracy = accuracy
+        best_thresh = model.threshold
+print max_accuracy,best_thresh
+model.threshold = best_thresh
 accuracy  = model.test(model.X_test, model.Y_test, model.testsize, Beta)
-
-print "accuracy ", accuracy
+print "Test accuracy"
+print accuracy
 def plot_learning_curve(costList, n_epoch):
     print "length of costList ", len(costList), " n_epoch ", n_epoch
     plt.plot(np.arange(n_epoch), np.array(costList))
